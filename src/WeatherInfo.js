f9e063b8bb3113a706c86a683b238da0c7419797
AshJungers <97846504+AshJungers@users.noreply.github.com>
2022-05-13 11:41:41 -0600

    Included weather icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 import "./Weather.css";
 
 export default function WeatherInfo(props) {
@@ -21,11 +22,10 @@ export default function WeatherInfo(props) {
             <h6 className="humidity">Humidity: {props.data.humidity}%</h6>
           </div>
           <div className="col-6 box-2">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="weatherPic"
-            />
+            <div className="WeatherPic">
+              <WeatherIcon code={props.data.icon} />
+            </div>
+
             <br />
             <h3 className="currentTemp">
               {Math.round(props.data.temperature)}
