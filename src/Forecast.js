15b5ddef36772f4423d23b9e8b77accf632a00e8
AshJungers <97846504+AshJungers@users.noreply.github.com>
2022-05-15 15:10:03 -0600

    Updated class to className

--- src/Forecast.js
+++ src/Forecast.js
@@ -3,97 +3,102 @@ import "./Weather.css";
 
 export default function Forecast() {
   return (
-    <div class="row justify-content-evenly">
-      <div class="col-sm-2">
-        <div class="card">
-          <div class="card-body shadow">
-            <h5 class="card-title">Monday</h5>
+    <div className="row justify-content-evenly">
+      <div className="col-sm-2">
+        <div className="card">
+          <div className="card-body shadow">
+            <h5 className="card-title">Monday</h5>
 
             <hr />
             <img
               src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
               alt="weatherPic"
             />
-            <span class="weather-forecast-temperature">
-              <p class="card-text">
-                <span class="weather-forecast-temperature-max">55°</span> | {""}
-                <span class="weather-forecast-temperature-min">44°</span>
+            <span className="weather-forecast-temperature">
+              <p className="card-text">
+                <span className="weather-forecast-temperature-max">55°</span> |{" "}
+                {""}
+                <span className="weather-forecast-temperature-min">44°</span>
               </p>
             </span>
           </div>
         </div>
       </div>
-      <div class="col-sm-2">
-        <div class="card">
-          <div class="card-body shadow">
-            <h5 class="card-title">Tuesday</h5>
+      <div className="col-sm-2">
+        <div className="card">
+          <div className="card-body shadow">
+            <h5 className="card-title">Tuesday</h5>
 
             <hr />
             <img
               src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
               alt="weatherPic"
             />
-            <span class="weather-forecast-temperature">
-              <p class="card-text">
-                <span class="weather-forecast-temperature-max">55°</span> | {""}
-                <span class="weather-forecast-temperature-min">44°</span>
+            <span className="weather-forecast-temperature">
+              <p className="card-text">
+                <span className="weather-forecast-temperature-max">55°</span> |{" "}
+                {""}
+                <span className="weather-forecast-temperature-min">44°</span>
               </p>
             </span>
           </div>
         </div>
       </div>
-      <div class="col-sm-2">
-        <div class="card">
-          <div class="card-body shadow">
-            <h5 class="card-title">Wednesday</h5>
+      <div className="col-sm-2">
+        <div className="card">
+          <div className="card-body shadow">
+            <h5 className="card-title">Wednesday</h5>
 
             <hr />
             <img
               src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
               alt="weatherPic"
             />
-            <span class="weather-forecast-temperature">
-              <p class="card-text">
-                <span class="weather-forecast-temperature-max">55°</span> | {""}
-                <span class="weather-forecast-temperature-min">44°</span>
+            <span className="weather-forecast-temperature">
+              <p className="card-text">
+                <span className="weather-forecast-temperature-max">55°</span> |{" "}
+                {""}
+                <span className="weather-forecast-temperature-min">44°</span>
               </p>
             </span>
           </div>
         </div>
       </div>
-      <div class="col-sm-2">
-        <div class="card">
-          <div class="card-body shadow">
-            <h5 class="card-title">Thursday</h5>
+      <div className="col-sm-2">
+        <div className="card">
+          <div className="card-body shadow">
+            <h5 className="card-title">Thursday</h5>
 
             <hr />
             <img
               src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
               alt="weatherPic"
             />
-            <span class="weather-forecast-temperature">
-              <p class="card-text">
-                <span class="weather-forecast-temperature-max">55°</span> | {""}
-                <span class="weather-forecast-temperature-min">44°</span>
+            <span className="weather-forecast-temperature">
+              <p className="card-text">
+                <span className="weather-forecast-temperature-max">55°</span> |{" "}
+                {""}
+                <span className="weather-forecast-temperature-min">44°</span>
               </p>
             </span>
           </div>
         </div>
       </div>
-      <div class="col-sm-2">
-        <div class="card">
-          <div class="card-body shadow">
-            <h5 class="card-title">Friday</h5>
+      <div className="col-sm-2">
+        <div className="card">
+          <div className="card-body shadow">
+            <h5 className="card-title">Friday</h5>
 
             <hr />
             <img
               src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
               alt="weatherPic"
             />
-            <span class="weather-forecast-temperature">
-              <p class="card-text">
-                <span class="weather-forecast-temperature-max">55°</span> | {""}
-                <span class="weather-forecast-temperature-min">44°</span>
+            <span className="weather-forecast-temperature">
+              <p className="card-text">
+                <span className="weather-forecast-temperature-max">55°</span> |{" "}
+                {""}
+                <span className="weather-forecast-temperature-min">44°</span>
               </p>
             </span>
           </div>
